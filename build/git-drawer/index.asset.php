049c7054d4a4cb5f6193ca3e048011e68942d69f
Mohammad Obayed Mamur <obayedmamur@gmail.com>
2026-02-26 11:03:03 +0600

    Add generated build assets and update .gitignore to track them.

---
+++ build/git-drawer/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'ef8196b082d0bc600b0a');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'd854976b0728090d2b1b');
